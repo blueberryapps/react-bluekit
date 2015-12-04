e363586888289bac1443b48dc7ac48b8269e7be9
Ondrej Bartas <ondrej@bartas.cz>
2015-12-04 13:00:07 +0100

    Enable extend/enhance component library props from component

--- src/extendProps.js
+++ src/extendProps.js
@@ -1,10 +1,18 @@
 import deepMerge from './deepMerge';
 
-export default function extendProps(defaultProps, customProps, component, propsDefinition, handleChange) {
+export default function extendProps(defaultProps, customProps, component, propsDefinition, library) {
   const override = {}
 
   if (propsDefinition.onChange && propsDefinition.value)
-    override.onChange = handleChange('value', propsDefinition.value.type.name)
+    override.onChange = library.createHandleChange(library)('value', propsDefinition.value.type.name)
 
-  return deepMerge(defaultProps, customProps, override)
+  const enhance = component.enhanceComponentLibraryDefaults
+    ? component.enhanceComponentLibraryDefaults(override, library)
+    : override
+
+  const extend = component.extendComponentLibraryProps
+    ? component.extendComponentLibraryProps(enhance, library)
+    : enhance
+
+  return deepMerge(defaultProps, customProps, extend)
 }
