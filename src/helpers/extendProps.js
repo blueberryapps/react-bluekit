4885f3f41dbf60b08971b8c8ca3742872b8f4cb9
Ondrej Bartas <ondrej@bartas.cz>
2016-05-03 16:42:54 +0200

    Implementation of most prop types

--- src/helpers/extendProps.js
+++ src/helpers/extendProps.js
@@ -6,13 +6,21 @@ export default function extendProps({defaultProps, customProps, component, props
   if (propsDefinition.onChange && propsDefinition.value)
     override.onChange = createSetAtomProp('value', propsDefinition.value.type.name)
 
-  const enhance = component.enhanceComponentLibraryDefaults
+  const enhanceOld = component.enhanceComponentLibraryDefaults
     ? component.enhanceComponentLibraryDefaults(override, setAtomProp)
     : override
 
-  const extend = component.extendComponentLibraryProps
-    ? component.extendComponentLibraryProps(enhance, setAtomProp)
-    : enhance
+  const enhanceBluekit = component.enhanceBluekitDefaults
+    ? component.enhanceBluekitDefaults(enhanceOld, setAtomProp)
+    : enhanceOld
 
-  return deepMerge(defaultProps, customProps, extend)
+  const extendOld = component.extendComponentLibraryProps
+    ? component.extendComponentLibraryProps(enhanceBluekit, setAtomProp)
+    : enhanceBluekit
+
+  const extendBluekit = component.extendBluekitProps
+    ? component.extendBluekitProps(extendOld, setAtomProp)
+    : extendOld
+
+  return deepMerge(defaultProps, customProps, extendBluekit)
 }
