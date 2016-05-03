40297550502ad19e812617b1d51aede4e205e37a
Ondrej Bartas <ondrej@bartas.cz>
2016-05-03 11:49:45 +0200

    Add SVG Icon

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+const configureSvgIcon = require('react-svg-icon-generator').default;
+
+configureSvgIcon({
+  destination: path.join(__dirname, 'src', 'app', 'atoms', 'Icon.react.js'),
+  svgDir: path.join(__dirname, 'src', 'icons')
+});
