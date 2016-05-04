114f0f9d558690a5e86d4798b75946b94b3ff734
Ondrej Bartas <ondrej@bartas.cz>
2016-05-04 16:47:15 +0200

    Fix node and element preview

--- src/helpers/extendComponentProps.js
+++ src/helpers/extendComponentProps.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Map, fromJS} from 'immutable';
+import {fromJS} from 'immutable';
 
 export default function extendComponentProps(buildedProps, propsDefinition) {
   const immutableBuildedProps = fromJS(buildedProps)
   const componentProps = {}
 
-  if (propsDefinition.children)
+  if (propsDefinition.get('children'))
     componentProps.children = <span dangerouslySetInnerHTML={{__html: (immutableBuildedProps.get('children') || 'DEFAULT CHILDREN')}} />
 
-  Map(propsDefinition).map((data, prop) => {
-    if (data.type && (data.type.name === 'node' || data.type.name === 'element'))
+  propsDefinition.map((data, prop) => {
+    const name = data.getIn(['type', 'name'])
+    if (name === 'node' || name === 'element')
       componentProps[prop] = immutableBuildedProps.get(prop) ? <span dangerouslySetInnerHTML={{__html: immutableBuildedProps.get(prop)}} /> : ''
   })
 
