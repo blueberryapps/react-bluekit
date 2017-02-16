c5264aa232591c61ea28cbb3ba65afd52f447c82
Ondrej Bartas <ondrej@bartas.cz>
2017-02-16 09:38:18 +0100

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface BluekitProps {
+  componentsIndex: Object;
+  inline?: boolean;
+}
+
+export default class Bluekit extends React.Component<BluekitProps, any> {
+
+}
